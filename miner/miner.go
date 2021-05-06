4cf9569d1aaeb381a798c81394800b8d7a3d6b1b
Joshua Gutow <joshua@clabs.co>
2021-05-06 16:39:35 -0400

    Pull out recover randomness cache function

--- miner/miner.go
+++ miner/miner.go
@@ -87,6 +87,42 @@ func New(eth Backend, config *Config, chainConfig *params.ChainConfig, mux *even
 	return miner
 }
 
+// recoverRandomnessCache checks and potnetially recovers the randomness cache for istanbul engines
+func (miner *Miner) recoverRandomnessCache() {
+	// If this is using the istanbul consensus engine, then we need to check
+	// for the randomness cache for the randomness beacon protocol
+	_, isIstanbul := miner.engine.(consensus.Istanbul)
+	if isIstanbul {
+		// getCurrentBlockAndState
+		currentBlock := miner.eth.BlockChain().CurrentBlock()
+		currentHeader := currentBlock.Header()
+		currentState, err := miner.eth.BlockChain().StateAt(currentBlock.Root())
+		if err != nil {
+			log.Error("Error in retrieving state", "block hash", currentHeader.Hash(), "error", err)
+			return
+		}
+
+		if currentHeader.Number.Uint64() > 0 {
+			// Check to see if we already have the commitment cache
+			lastCommitment, err := random.GetLastCommitment(miner.validator, currentHeader, currentState)
+			if err != nil {
+				log.Error("Error in retrieving last commitment", "error", err)
+				return
+			}
+
+			// If there is a non empty last commitment and if we don't have that commitment's
+			// cache entry, then we need to recover it.
+			if (lastCommitment != common.Hash{}) && (rawdb.ReadRandomCommitmentCache(miner.db, lastCommitment) == common.Hash{}) {
+				err := miner.eth.BlockChain().RecoverRandomnessCache(lastCommitment, currentBlock.Hash())
+				if err != nil {
+					log.Error("Error in recovering randomness cache", "error", err)
+					return
+				}
+			}
+		}
+	}
+}
+
 // update keeps track of the downloader events. Please be aware that this is a one shot type of update loop.
 // It's entered once and as soon as `Done` or `Failed` has been broadcasted the events are unregistered and
 // the loop is exited. This to prevent a major security vuln where external parties can DOS you with blocks
@@ -126,38 +162,7 @@ func (miner *Miner) update() {
 					miner.worker.start()
 				}
 			case downloader.DoneEvent:
-				// If this is using the istanbul consensus engine, then we need to check
-				// for the randomness cache for the randomness beacon protocol
-				_, isIstanbul := miner.engine.(consensus.Istanbul)
-				if isIstanbul {
-					// getCurrentBlockAndState
-					currentBlock := miner.eth.BlockChain().CurrentBlock()
-					currentHeader := currentBlock.Header()
-					currentState, err := miner.eth.BlockChain().StateAt(currentBlock.Root())
-					if err != nil {
-						log.Error("Error in retrieving state", "block hash", currentHeader.Hash(), "error", err)
-						return
-					}
-
-					if currentHeader.Number.Uint64() > 0 {
-						// Check to see if we already have the commitment cache
-						lastCommitment, err := random.GetLastCommitment(miner.validator, currentHeader, currentState)
-						if err != nil {
-							log.Error("Error in retrieving last commitment", "error", err)
-							return
-						}
-
-						// If there is a non empty last commitment and if we don't have that commitment's
-						// cache entry, then we need to recover it.
-						if (lastCommitment != common.Hash{}) && (rawdb.ReadRandomCommitmentCache(miner.db, lastCommitment) == common.Hash{}) {
-							err := miner.eth.BlockChain().RecoverRandomnessCache(lastCommitment, currentBlock.Hash())
-							if err != nil {
-								log.Error("Error in recovering randomness cache", "error", err)
-								return
-							}
-						}
-					}
-				}
+				miner.recoverRandomnessCache()
 				canStart = true
 				if shouldStart {
 					miner.worker.start()
